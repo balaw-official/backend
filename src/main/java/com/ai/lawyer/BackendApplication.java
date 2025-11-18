497512b227906277df6faf38f3a8e05da411c1cb
dhj <doohj1218@naver.com>
2025-11-18 10:31:47 +0900

    init infra

---
+++ src/main/java/com/ai/lawyer/BackendApplication.java
@@ -0,0 +1,19 @@
+package com.ai.lawyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@SpringBootApplication
+@EnableJpaAuditing
+@ConfigurationPropertiesScan
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
